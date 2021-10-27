75c851f29ae26d052a413ccd1ca33e28d479a404
Micheal Earl <michealearldev@gmail.com>
2021-10-27 18:50:26 -0400

    Reset frontend and server to before Plaid

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+app.use(express.json());
 
 // Connect to the database
 connectDB();
@@ -11,6 +12,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+// axios request
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
