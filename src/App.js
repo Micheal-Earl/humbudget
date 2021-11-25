c240cdd3263f8d683d9a8468a026c04049e37f71
Micheal Earl <michealearldev@gmail.com>
2021-11-25 17:58:29 -0500

    Added simple graph using dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { db } from "./firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+//import * as V from "victory";
+import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
 import Authentication from "./components/Authentication.js";
 import CreateTransaction from "./components/CreateTransaction.js";
 import UpdateTransaction from "./components/UpdateTransaction.js";
 import DeleteTransaction from "./components/DeleteTransaction.js";
 
-import { collection, getDocs } from "firebase/firestore";
 import "./App.css";
 
 function App() {
@@ -100,9 +102,43 @@ function App() {
       <div className="App">
         <h1 className="h1">HumBudget</h1>
         <Authentication setSuperUser={setSuperUser} />
+        <Graph />
       </div>
     );
   }
+}
+
+function Graph() {
+  // Dummy data for graph
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
+
+  return (
+    <div className="w-1/4">
+      <VictoryChart
+        // domainPadding will add space to each side of VictoryBar to
+        // prevent it from overlapping the axis
+        domainPadding={20}
+      >
+        <VictoryAxis
+          // tickValues specifies both the number of ticks and where
+          // they are placed on the axis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          // tickFormat specifies how ticks should be displayed
+          tickFormat={(x) => `$${x / 1000}k`}
+        />
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
+    </div>
+  );
 }
 
 export default App;
